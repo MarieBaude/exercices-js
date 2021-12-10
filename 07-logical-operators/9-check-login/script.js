a268f578e1bfb5b8dd062af2497f8a04c0a967e0
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-10 17:39:40 +0100

    delete exercise no ask

--- 07-logical-operators/9-check-login/script.js
+++ 07-logical-operators/9-check-login/script.js
@@ -1,7 +1,19 @@
+'use strict';
 
+let user = prompt('Nom ?');
 
-if (condition) {
-      
+if (user == 'Admin') {
+      let passWord = prompt('Mot de passe ?'); {
+            if (passWord == "TheMaster") {
+                  alert('Welcome!');
+            } else if (passWord === ''|| passWord === null){ 
+                  alert('Canceled');
+            } else {
+                  alert('Wrong password');
+            }
+      }
+} else if (user === ''|| user === null){
+      alert('Canceled');
 } else {
-      
+      alert("I don't know you")
 }
